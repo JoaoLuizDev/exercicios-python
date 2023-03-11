a0a3920f93a35a0857d49c8abf702d46aa5960f9
João Luiz <93620657+JoaoLuizDev@users.noreply.github.com>
2023-03-10 22:29:10 -0300

    Create 002.py

---
+++ resolucoes/002.py
@@ -0,0 +1,29 @@
+"""
+Faça um Programa que peça um número e então mostre a mensagem:
+    O número informado foi [número].
+"""
+
+numero = input("Digite um número: ")
+print(f"O número informado é {numero}.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
