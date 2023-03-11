25dcf3d87a331ea2a6b1182b9cea38865f66cbe9
João Luiz <93620657+JoaoLuizDev@users.noreply.github.com>
2023-03-10 22:48:56 -0300

    Update 003.py

--- resolucoes/003.py
+++ resolucoes/003.py
@@ -1,3 +1,8 @@
 """
 Faça um Programa que peça dois números e imprima a soma.
 """
+
+numero1 = int(input("Digite um número qualquer: "))
+numero2 = int(input("Digite outro número qualquer: "))
+soma = numero1 + numero2
+print(f"A soma de {numero1} e {numero2} é {soma}")
