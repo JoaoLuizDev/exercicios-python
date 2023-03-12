85d5dbd8a07594af8e0f021b55dd9a05d10863a3
João Luiz <93620657+JoaoLuizDev@users.noreply.github.com>
2023-03-11 22:44:35 -0300

    Update 004.py

--- resolucoes/004.py
+++ resolucoes/004.py
@@ -2,12 +2,10 @@
 Faça um Programa que peça as 4 notas bimestrais e mostre a média.
 """
 
-nota1 = float(input("Digite a nota do 1º bimestre: ")
-nota1 = float(input("Digite a nota do 2º bimestre: ")
-nota3 = float(input("Digite a nota do 3º bimestre: ")
-nota4 = float(input("Digite a nota do 4º bimestre: ")
+nota1 = float(input("Digite a nota do 1º bimestre: "))
+nota2 = float(input("Digite a nota do 2º bimestre: "))
+nota3 = float(input("Digite a nota do 3º bimestre: "))
+nota4 = float(input("Digite a nota do 4º bimestre: "))
 media = (nota1 + nota2 + nota3 + nota4) / 4
-print(
-  f"As notas bimestrais foram {nota1:.2f}, {nota2:.2f}, {nota3:.2f} e {nota4:.2f}.  
-  "A nota média foi de {media:.2f}."
-)
+print(f"As notas bimestrais foram {nota1:.2f}, {nota2:.2f}, {nota3:.2f} e {nota4:.2f}."
+      f"\nA nota média foi de {media:.2f}.")
