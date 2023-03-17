1d24c59a903d335692748c09d0273baebd0668b6
João Luiz <93620657+JoaoLuizDev@users.noreply.github.com>
2023-03-16 22:55:30 -0300

    Create 006.py

---
+++ resolucoes/006.py
@@ -0,0 +1,11 @@
+print("Vamos calcular a área do círculo.")
+raio = float(input("Para isso, digite o raio do círculo: "))
+π = 3,14
+r = raio ** 2
+area = 3.14 * (r ** 2)
+print(f"O círculo que tem o raio igual a {raio:.2f}, terá a área de {area:.2f}.")
+
+print("\nÁrea = π * r²")
+print(f"Área = {π} * {raio:.2f}²")
+print(f"Área = {π} * {r:.2f}")
+print(f"Área = {area:.2f}")
