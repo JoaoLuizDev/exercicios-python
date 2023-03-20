d9b8cb6a799b6f9f81dfe6a8c3d99fd4b9a30108
João Luiz <93620657+JoaoLuizDev@users.noreply.github.com>
2023-03-19 22:03:17 -0300

    Update 003.py

--- Curso Python - Guanabara/Mundo 1/003.py
+++ Curso Python - Guanabara/Mundo 1/003.py
@@ -1,2 +1,6 @@
-# Crie um script Python que leia dois números e tente mostrar a soma entre eles
+# Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo e todas as informações possíveis sobre ela
+
+
+
+#Usando if e else "sim e não."
 
