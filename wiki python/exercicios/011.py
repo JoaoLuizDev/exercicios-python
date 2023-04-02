7365ac3eb33d1acfd772cc27053efda32c111de0
João Luiz <93620657+JoaoLuizDev@users.noreply.github.com>
2023-04-02 19:57:56 -0300

    Update 011.py

--- wiki python/exercicios/011.py
+++ wiki python/exercicios/011.py
@@ -1,6 +1,8 @@
 """
-Faça um Programa que peça 2 números inteiros e um número real. Calcule e mostre:
-o produto do dobro do primeiro com metade do segundo .
-a soma do triplo do primeiro com o terceiro.
-o terceiro elevado ao cubo.
+Faça um Programa que peça 2 números inteiros e um número real. 
+
+Calcule e mostre:
+  - o produto do dobro do primeiro com metade do segundo .
+  - a soma do triplo do primeiro com o terceiro.
+  - o terceiro elevado ao cubo.
 """
