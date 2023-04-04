ad7248e70f11b4b9c5099db8f3a50a2497f0cddf
João Luiz <93620657+JoaoLuizDev@users.noreply.github.com>
2023-04-03 21:30:44 -0300

    Update 018.py

--- wiki python/resolucoes/018.py
+++ wiki python/resolucoes/018.py
@@ -11,3 +11,15 @@ segundos = tempos % 60
 print("A fórmula para calcular é:\n"
       "Tamanho do arquivo em megabytes / (velocidade de download em megabits / 8) =  tempo em segundos.\n")
 print(f"O tempo para completar o download será de {minutos:.0f} minutos e {segundos:.0f} segundos.")
+
+
+"""
+Observação:
+            minutos = tempo // 60 ------- esta linha calcula a quantidade de minutos no tempo estimado de download, 
+                                          dividindo o valor armazenado na variável tempos por 60 e arredondando para 
+                                          baixo (usando o operador "//" para divisão de números inteiros).
+            
+            segundos = tempos % 60 ------ esta linha calcula o número de segundos restantes no tempo estimado de download, 
+                                          calculando o resto da divisão do valor armazenado na variável tempos por 60 
+                                          (usando o operador "%" para módulo).
+"""
