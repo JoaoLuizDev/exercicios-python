83f16b84ea0bfda9ce24030b5f8f529ea70db555
João Luiz <93620657+JoaoLuizDev@users.noreply.github.com>
2023-04-03 21:37:01 -0300

    Create 019.py

---
+++ wiki python/resolucoes/019.py
@@ -0,0 +1,8 @@
+n1 = float(input("Digite um número: "))
+n2 = float(input("Digite outro número: "))
+if n1 > n2:
+  print(n1)
+elif n2 > n1:
+  print(n2)
+else:
+  print("Os números tem o mesmo valor.")
