72dd60b220d2303791c8f700cb9171fac87d9fc7
João Luiz <93620657+JoaoLuizDev@users.noreply.github.com>
2023-04-03 22:07:22 -0300

    Create 001.py

---
+++ Curso Python - Guanabara/Mundo 1/Resoluções/001.py
@@ -0,0 +1,12 @@
+# Crie um programa que escreva "Olá, mundo" na tela.
+
+print("Olá, mundo.")
+print("Hello, world.")
+mensagem = "olá, mundo."
+print(mensagem)
+
+# Crie um script Python que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas de acordo com o valor digitado
+
+nome = str(input("Qual seu nome? "))
+print(f"Olá, {nome}, seja bem-vindo.")
+
