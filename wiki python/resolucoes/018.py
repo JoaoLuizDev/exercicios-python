7c04802de37c6bd0a4bf79c507e3910fef8d390e
João Luiz <93620657+JoaoLuizDev@users.noreply.github.com>
2023-04-03 21:23:43 -0300

    Update 018.py

--- wiki python/resolucoes/018.py
+++ wiki python/resolucoes/018.py
@@ -3,13 +3,11 @@ Faça um programa que peça o tamanho de um arquivo para download (em MB) e a ve
 calcule e informe o tempo aproximado de download do arquivo usando este link (em minutos).
 """
 
-import math
-
 tamanho = float(input("Informe o tamanho do arquivo, em MB, que você deseja fazer download: "))
 velocidade = float(input("Informe, em Mbps, a velocidade da sua conexão:"))
 tempo = tamanho / (velocidade / 8)
-minutos = tempo / 60
-minutos_arredondados = math.ceil(minutos)
+minutos = tempo // 60
+segundos = tempos % 60
 print("A fórmula para calcular é:\n"
       "Tamanho do arquivo em megabytes / (velocidade de download em megabits / 8) =  tempo em segundos.\n")
-print(f"O tempo para completar o download será de {tempo:.2f} segundos, ou, arredondando para cima, {minutos_arredondados} minutos.")
+print(f"O tempo para completar o download será de {minutos:.0f} minutos e {segundos:.0f} segundos.")
