fe57f88310150a1209510275ab094b76c4b83d58
João Luiz <93620657+JoaoLuizDev@users.noreply.github.com>
2023-04-14 20:40:00 -0300

    Update 001.py

--- Curso Python - Guanabara/Mundo 1/001.py
+++ Curso Python - Guanabara/Mundo 1/001.py
@@ -1,5 +1,3 @@
 """ 
-Crie um script Python que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas de acordo com o valor digitado
-
 Crie um programa que escreva "Olá, mundo" na tela.
 """
