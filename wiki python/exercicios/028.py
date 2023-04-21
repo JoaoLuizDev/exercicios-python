97483c4ae5f963c03f0d4f4825e9f2abb0fd2609
João Luiz <93620657+JoaoLuizDev@users.noreply.github.com>
2023-04-21 11:38:31 -0300

    Update 028.py

--- wiki python/exercicios/028.py
+++ wiki python/exercicios/028.py
@@ -7,14 +7,3 @@ Peça para digitar:
 
 Imprima a mensagem "Bom Dia!", "Boa Tarde!" ou "Boa Noite!" ou "Valor Inválido!", conforme o caso.
 """
-
-print("Preciso saber em qual turno você estuda.\n  M- matutino;\n  V- vespertino;\n  N- noturno.\n")
-turno = str(input("Digite a letra que corresponde ao seu turno:")).upper().strip()
-if turno == "M":
-  print("Bom dia.")
-elif turno == "V":
-  print("Boa tarde.")
-elif turno == "N":
-  print("Boa noite.")
-else:
-  print("Valor inválido.")
