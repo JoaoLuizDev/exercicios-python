5fb85e4a61817c4c73dea9ad826d7dda2b85854d
“João <joaoluizcienciacomp@gmail.com>
2023-05-01 11:09:21 -0300

    Mais resoluções

--- wiki python/resolucoes/042.py
+++ wiki python/resolucoes/042.py
@@ -5,10 +5,43 @@ As perguntas são:
     "Esteve no local do crime?"
     "Mora perto da vítima?"
     "Devia para a vítima?"
-    "Já trabalhou com a vítima?"
+    ""
 O programa deve no final emitir uma classificação sobre a participação
 da pessoa no crime.
 Se a pessoa responder positivamente a 2 questões ela deve ser classificada
 como "Suspeita", entre 3 e 4 como "Cúmplice" e 5 como "Assassino".
 Caso contrário, ele será classificado como "Inocente".
 """
+
+print("Olá, esse programa simula um interrogatório.\n"
+      "Após as perguntas, será emitido uma classificação sobre a participação do usuário no crime.\n"
+      "Para responder 'SIM', digite 1. Para responder 'NÃO', digite 0.")
+p1 = int(input("Telefonou para a vítima?"))
+if p1 == 0 or 1:
+    p2 = int(input("Esteve no local do crime?"))
+    if p2 == 0 or 1:
+        p3 = int(input("Mora perto da vítima?"))
+        if p3 == 0 or 1:
+            p4 = int(input("Devia para a vítima?"))
+            if p4 == 0 or 1:
+                p5 = int(input("Já trabalhou com a vítima?"))
+                if p5 == 0 or 1:
+                    soma = p1 + p2 + p3 + p4 + p5
+                    if soma == 1:
+                        print("Você é INOCENTE.")
+                    elif soma == 2:
+                        print("Você é considerado SUSPEITO.")
+                    elif soma == 3 or 4:
+                        print("Você é considerado CÚMPLICE.")
+                    else:
+                        print("Você é o ASSASSINO.")
+                else:
+                    print("ERRO")
+            else:
+                print("ERRO")
+        else:
+            print("ERRO")
+    else:
+        print("ERRO")    
+else:
+    print("ERRO")
--- wiki python/resolucoes/043.py
+++ wiki python/resolucoes/043.py
@@ -11,3 +11,38 @@ o tipo de combustível (codificado da seguinte forma: A-álcool, G-gasolina),
 calcule e imprima o valor a ser pago pelo cliente sabendo-se que o preço do
 litro da gasolina é R$ 2,50 o preço do litro do álcool é R$ 1,90.
 """
+
+tipo = str(input("Qual o tipo de combustível?\nDigite G para gasolina e A para álcool: "))
+litros = float(input("Digite a quantidade de litros vendida: "))
+if tipo == "G":
+    if 0 < litros <= 20:
+        desconto = 2.5 - (2.5 / 100 * 3)
+        valor = litros * desconto
+        preço = 2.5 * litros
+        economia = preço - valor
+        print(f"O valor sem desconto seria de R$ {preço:.2f}.\nDesconto: R$ {economia:.2f}.\n\nTOTAL: R$ {valor:.2f}.")
+    elif litros > 20:
+        desconto = 2.5 - (2.5 / 100 * 5)
+        valor = litros * desconto
+        preço = 2.5 * litros
+        economia = preço - valor
+        print(f"O valor sem desconto seria de R$ {preço:.2f}.\nDesconto: R$ {economia:.2f}.\n\nTOTAL: R$ {valor:.2f}.")
+    else:
+        print("ERRO")
+elif tipo == "A":
+    if 0 < litros <= 20:
+        desconto = 1.9 - (1.9 / 100 * 4)
+        valor = litros * desconto
+        preço = 1.9 * litros
+        economia = preço - valor
+        print(f"O valor sem desconto seria de R$ {preço:.2f}.\nDesconto: R$ {economia:.2f}.\n\nTOTAL: R$ {valor:.2f}.")
+    elif litros > 20:
+        desconto = 1.9 - (1.9 / 100 * 6)
+        valor = litros * desconto
+        preço = 1.9 * litros
+        economia = preço - valor
+        print(f"O valor sem desconto seria de R$ {preço:.2f}.\nDesconto: R$ {economia:.2f}.\n\nTOTAL: R$ {valor:.2f}.")
+    else:
+        print("ERRO")
+else:
+    print("ERRO")
