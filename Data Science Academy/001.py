8c1f554e21849105d6f2ad196a9e344fa09dbe4b
João Luiz <93620657+JoaoLuizDev@users.noreply.github.com>
2023-07-10 08:36:06 -0300

    Update 001.py

--- Data Science Academy/001.py
+++ Data Science Academy/001.py
@@ -1,2 +1,4 @@
-""" Exercício 1 - Crie uma estrutura que pergunte ao usuário qual o dia da semana. Se o dia for igual a Domingo ou igual a sábado, 
-imprima na tela "Hoje é dia de descanso", caso contrário imprima na tela "Você precisa trabalhar!"
+""" 
+Crie uma estrutura que pergunte ao usuário qual o dia da semana. Se o dia for igual a Domingo ou igual a sábado, imprima na tela 
+"Hoje é dia de descanso", caso contrário imprima na tela "Você precisa trabalhar!"
+"""
