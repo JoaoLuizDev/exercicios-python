97aa3af26cbe0ff41ec7cf7b3527718a66aae009
João Luiz <93620657+JoaoLuizDev@users.noreply.github.com>
2023-07-10 08:49:32 -0300

    Create 010.py

---
+++ Data Science Academy/010.py
@@ -0,0 +1,6 @@
+"""
+Faça um programa que conte quantas vezes a letra "r" aparece na frase abaixo. Use um placeholder na sua instrução de impressão
+
+“É melhor, muito melhor, contentar-se com a realidade; se ela não é tão brilhante como os sonhos, tem pelo menos a  vantagem de existir.” 
+(Machado de Assis)
+"""
