a454c3a98f44d94c593e51d150844fdeaf67bbb9
João Luiz <93620657+JoaoLuizDev@users.noreply.github.com>
2023-07-10 08:35:33 -0300

    Update 002.py

--- Data Science Academy/002.py
+++ Data Science Academy/002.py
@@ -1,1 +1,3 @@
-""" Exercício 2 - Crie uma lista de 5 frutas e verifique se a fruta 'Morango' faz parte da lista """
+""" 
+Crie uma lista de 5 frutas e verifique se a fruta 'Morango' faz parte da lista 
+"""
